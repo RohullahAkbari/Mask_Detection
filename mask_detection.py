dad1644785a8f5da27aa3fb4b4b4f04bd3ac5999
rohullahAkbari <akbarirohullah270@gmail.com>
2023-06-13 11:14:48 +0430

    first commit

---
+++ mask_detection.py
@@ -0,0 +1,40 @@
+## Code for real time prediction ##
+
+import cv2
+import tensorflow as tf
+import numpy as np
+import sys
+
+class_names = ['incorrect_mask', 'with_mask', 'without_mask']
+
+model = tf.keras.models.load_model('./model-14.hdf5')
+
+video = cv2.VideoCapture(0)
+
+faceDetect = cv2.CascadeClassifier('raw.githubusercontent.com_opencv_opencv_master_data_haarcascades_haarcascade_frontalface_default.xml')
+
+while True:
+    ret, frame = video.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = faceDetect.detectMultiScale(gray, 1.3, 3)
+
+    for x, y, w, h in faces:
+        sub_face_img = gray[y : y + h, x : x + w]
+        resized = cv2.resize(sub_face_img, (48, 48))
+        normalize = resized / 255.0
+        reshaped = np.reshape(normalize, (1, 48, 48, 1))
+        result = model.predict(reshaped)
+        label = np.argmax(result, axis=1)[0]
+        print(label)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 1)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (50, 50, 255), 2)
+        cv2.rectangle(frame, (x, y - 40), (x + w, y), (50, 50, 255), -1)
+        cv2.putText(frame, class_names[label], (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)
+    
+    cv2.imshow("Frame", frame)
+    k = cv2.waitKey(1)
+    if k == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
